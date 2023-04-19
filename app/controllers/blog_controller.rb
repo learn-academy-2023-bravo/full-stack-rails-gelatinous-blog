4c39ae7b5ba5c505221d82e951b6b22ef9cc74d9
bjohnwell <brandonjohnwell@gmail.com>
2023-04-19 16:35:02 -0700

    story 1 complete ready for show branch

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,17 @@
+class BlogController < ApplicationController
+    def index 
+        @blogs = Blog.all
+    end
+    def show
+        @blog = Blog.find(params[:id])
+    end
+    # def new
+    #     @blog = Blog.new
+    # end
+end
+
+
+# def index
+#     # active record query to see all the books in the db, stored in an instance variable
+#     @books = Book.all
+#   end
